f979ba3327a1d3d10357520786e6ddd7482677c2
Riddick <riddick.angiela@student.moringaschool.com>
2022-11-08 20:35:40 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+ import './App.css';
+// import ReviewCard from './components/ReviewCard';
+import About from './components/About'
+import Description from './components/Description';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar'
+import { Routes ,Route } from "react-router-dom";
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path={`/:id`} element={<ReviewCard />} /> */}
+        <Route path={`/:id`} element={<Description />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
