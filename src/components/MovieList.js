6ca2a3a4ccb9415b43d8e9109d647ded6dc1e634
mercylynn <mercy.kamene@student.moringaschool.com>
2022-11-09 10:43:43 +0300

    add movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import Moviecard from './Moviecard'
+import MovieCard from './MovieCard'
 
 import Search from './Search'
 
 function MovieList() {
   const [movies, setMovies] = useState([])
 
+
   useEffect(() => {
     fetch('http://localhost:9292/movies')
       .then((resp) => resp.json())
       .then((data) => {
         setMovies(data)
+
       })
   }, [])
-
-
 
 
   const [genre, setGenre] = useState("All")
@@ -36,39 +36,20 @@ function MovieList() {
     .filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()));
 
   const displayMovies = moviesToDisplay.map((movie) => {
-    return <Moviecard key={movie.id} movie={movie} />
-
-
+    return <MovieCard key={movie.id} movie={movie} />
 
 
   })
 
-
-
-
-
-
-
-
-
-
-
   return (
 
     <>
-
-
-
       <Search onCategoryChange={handleGenreChange} onSearchChange={handleSearchChange} search={search} />
       <div className='moviecontainer'>
         {displayMovies}
       </div>
 
     </>
-
-
-
-
 
   )
 
