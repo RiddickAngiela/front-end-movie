33d806d950c397ee46046048fc704bd6de2478a6
anwarismail <anwaarismail449@gmail.com>
2022-11-09 10:53:39 +0300

    create the description page

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,7 +4,28 @@ import { useParams } from 'react-router-dom'
 
 function Description() {
 
- 
+  let {id}=useParams();
+  const [filtered,setFiltered]=useState([])
+  useEffect (()=>{
+    fetch(`http://localhost:9292/movies/${id}`)
+    .then ((res)=>res.json())
+    .then ((movie)=>setFiltered(movie))
+    console.log (filtered.Title)
+  },[])
+  return (
+    <>
+
+    <div className='card-des'>
+      <img src={filtered.image} alt=" Movie Image"/>
+      <h2 style={{color:'red'}}>TITLE :  {filtered.title}</h2>
+      <h4>Release Date :  {filtered.date}</h4>
+      <h4>Genre : {filtered.genre}</h4>
+      <h4>Description :</h4>
+      <p>{filtered.description}</p>
+    </div>
+    </>
+  )
+
 }
 
 export default Description
