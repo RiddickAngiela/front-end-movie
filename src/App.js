a5939c929106dc0ce9bc6e6c156f9fb7264fc036
mercylynn <mercy.kamene@student.moringaschool.com>
2022-11-09 07:17:08 +0300

    import Browser router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, {useState, useEffect} from 'react'
- import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css';
 // import ReviewCard from './components/ReviewCard';
 import About from './components/About'
 import Description from './components/Description';
 import Footer from './components/Footer';
+import Footer from './components/MovieList';
 import Navbar from './components/Navbar'
-import { Routes ,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Movie-hub'
+  })
   return (
     <div>
       <Navbar />
@@ -17,6 +22,7 @@ function App() {
         <Route path="/about" element={<About />} />
         {/* <Route path={`/:id`} element={<ReviewCard />} /> */}
         <Route path={`/:id`} element={<Description />} />
+        <Route path="/movies" element={<MovieList />} />
       </Routes>
       <Footer />
     </div>
